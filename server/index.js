93177a9e37c1b22f66018b2224e7207add358e3e
Anthony Fojas <anthonyjfojas@gmail.com>
2019-09-23 14:12:26 -0700

    Implement local connection to photos gallery service

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
+require('newrelic');
 const express = require('express');
+
 const app = express();
 const port = 3000;
+const morgan = require('morgan');
 
-app.use('/listing/:id', express.static('public'))
+app.use('/listing/:id', express.static('./public'))
+app.use(morgan('dev'));
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/api/listing/:listingID', (req, res) => {
+  const { listingID } = req.params;
+  res.redirect(`http://localhost:3100/api/listing/${listingID}`)
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}...`))
